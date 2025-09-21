081ec0f2f30e75f0130c7ce0ddbae6f437305e0c
Fadil <nfadil.mu@gmail.com>
2025-09-21 11:10:58 +0700

    chore: base path config

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,27 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'kamus-kanji';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-    paths: {
-    	base: isProd ? `/${repoName}` : ''
-    },
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
 			fallback: 'index.html'
 		}),
+		paths: {
+			base:
+				process.env.GITHUB_REPOSITORY &&
+					!process.env.GITHUB_REPOSITORY.endsWith('.github.io')
+					? '/' + process.env.GITHUB_REPOSITORY.split('/')[1]
+					: ''
+		},
 		prerender: {
 			entries: ['/'],
-      		handleUnseenRoutes: 'warn'
+			handleUnseenRoutes: 'warn'
 		}
 	}
 };
